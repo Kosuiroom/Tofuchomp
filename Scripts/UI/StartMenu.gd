218eb231d0bf60c7113ed037cbb3673b24455027
Pontus <pontus.goth@outlook.com>
2022-12-03 20:06:02 +0100

    added start and pause menu

--- Scripts/UI/StartMenu.gd
+++ Scripts/UI/StartMenu.gd
@@ -3,29 +3,20 @@ extends Control
 onready var startbtn = $MenuItems/Start
 onready var Buttonsound = $Buttonsound
 
-
-
 func _ready():
 	startbtn.grab_focus()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	MusicController.playgame()
-	
 func _on_Start_pressed():
-	get_tree().change_scene("res://GameScene/GameWorld.tscn")
-
-
+	get_tree().change_scene("res://GameScene/Level01.tscn")
 
 func _on_Exit_pressed():
 	get_tree().quit()
 
-
-
 func _on_Start_focus_exited():
-	Buttonsound.play()
-
-
+#	Buttonsound.play()
+	pass
 
 func _on_Exit_focus_exited():
-	Buttonsound.play()
-
+#	Buttonsound.play()
+	pass
---
+++ UI/pausescene/pausescreen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func set_is_paused(val):
+	is_paused = val
+	get_tree().paused = is_paused
+	visible = is_paused
+
+func _on_Continue_pressed():
+	self.is_paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _on_Exit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://Main.tscn")
