dd9b279d8d2cbc4ea0130cb8d0f929334e1ac255
Pontus <pontus.goth@outlook.com>
2022-12-03 10:40:57 +0100

    added enemy generic scene and the first smallenemy

---
+++ Actors/Enemies/Enemy.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+export var velocity := Vector2()
+export var speed := 200
+
+func _process(delta):
+	translate(velocity.normalized() * speed * delta)
+
+#TODO: delete enemy when it exists the screen	
+#	if get_position_in_parent().y >= get_viewport_rect().size.height:
+#		queue_free()
