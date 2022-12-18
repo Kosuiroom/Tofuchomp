11ef623ca03c049c31c4ec3f00ae033dfbd1594e
Pontus <pontus.goth@outlook.com>
2022-12-18 11:00:34 +0100

    added deathsound and animation to smallenemy

--- Actors/Enemies/SmallEnemy/SmallEnemy.gd
+++ Actors/Enemies/SmallEnemy/SmallEnemy.gd
@@ -1,20 +1,18 @@
 extends Area2D
 onready var anim = $SpriteAnimationPlayer
-
+onready var deathsound = $deathsound
+onready var colshape = $hitbox
 export var velocity := Vector2()
 export var speed := 200
 export var armor: = 2 setget set_armor
 
 func _process(delta):
 	translate(velocity.normalized() * speed * delta)
-	if armor <= 0:
-		print("small enemy died")
-		anim.play("Death")
 
 func _on_smallenemy_body_entered(body):
 	if body.is_in_group("player"):
 		body.armor -= 1
-		queue_free()
+		anim.play("Death")
 
 
 func _on_smallenemy_area_entered(area):
@@ -28,7 +26,15 @@ func _on_smallenemy_area_entered(area):
 func set_armor(value):
 	armor = value
 	if armor <= 0:
-		queue_free()
+		deathsound.play()
+		colshape.set_deferred("disabled", true)
+		anim.play("Death")
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_SpriteAnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Death":
+		EventBus.disconnect("player_laser_hit",self,"_on_player_laser_hit")
+		queue_free()
